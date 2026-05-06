48f9a793f3c8a1b4c065d379c1c98f72ca57c73f
AtalaBaris <barisatala@outlook.com>
2026-05-06 04:35:08 +0300

    Merge main into menu-service

---
+++ frontend/.next/dev/static/chunks/Desktop_QResto_frontend_src_app_page_0_sk8-h.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0p1k_next_dist_000m3d-._.js"],
+    source: "dynamic"
+});
